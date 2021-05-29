5c8e8fa81ad0cc101a3c953c5ed652dd407ffcc7
Sara Green <smg105c@gmail.com>
2021-05-29 14:12:52 -0700

    added button handler funtion to display all results

--- src/results/results.js
+++ src/results/results.js
@@ -8,14 +8,50 @@ class Results extends React.Component {
             items: [],
             error: null
         }
-    }   
+    }
+
+    handleClick = () => {
+        fetch('https://fetch-hiring.s3.amazonaws.com/hiring.json')
+            .then(res => res.json())
+            .then(
+                (result) => {
+                this.setState({
+                    items: result
+                });
+                },
+                (error) => {
+                this.setState({
+                    error
+                });
+                }
+            )
+    }
 
     render() {
-        return (
-            <section>
-                <button>Click Me</button>
-            </section>
-        ) 
+        const { error, items } = this.state
+
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        }
+
+        else {
+            return (
+                <section>
+                    <button onClick={this.handleClick}>Get Items</button>
+                    <ul>
+                    {items.map(item => (
+                        <li key={item.id}>
+                        <div>Item Id: {item.id}</div>
+                        <div>List Id: {item.listId}</div>
+                        <div>Name: {item.name}</div>
+                        </li>
+                    ))}
+                    </ul>
+                </section>
+            )
+
+        }
+         
     }
 }
 
