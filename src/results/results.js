5b42017f8d76a70361889e04e021e75744e805d0
Sara Green <smg105c@gmail.com>
2021-05-30 15:24:45 -0700

    cleaning up code and adding styles

--- src/results/results.js
+++ src/results/results.js
@@ -17,19 +17,20 @@ class Results extends React.Component {
             .then(res => res.json())
             .then(
                 (result) => { 
-                this.setState({
-                    items: result
-                });
+                    this.setState({
+                        items: result
+                    })
                 },
                 (error) => {
-                this.setState({
-                    error
-                });
+                    this.setState({
+                        error
+                    })
                 }
             )
     }
 
     handleBack = () => {
+        // refreshes page on back button click
         window.location.reload()
     }
 
@@ -55,13 +56,14 @@ class Results extends React.Component {
                 })
 
         // group list by listId
-
         sortedList.forEach(item => {
+            // creates a dynamic array containing all list ids
             if(this.state.listIds.indexOf(item.listId) === -1) {
                 this.state.listIds.push(item.listId)
             }
         })
 
+        // generates a list component for each list id
         let lists = this.state.listIds.map((id, i) => {
             return (
                 <List
@@ -72,16 +74,23 @@ class Results extends React.Component {
             )
         })
 
+        // error handling
+        if (error) {
+            return (
+                <div>
+                    <h2>Something went wrong, try again later</h2>
+                </div>
+            )
+        }
 
+        // no error
         return (
             <div>
                 <button className={(items.length === 0 ? 'show'  : 'hidden' )} onClick={this.handleClick}>Get Items</button>                  
                 <button className={(items.length === 0 ?  'hidden' : 'show' )} onClick={this.handleBack}>Back</button>
                 <div className="list-container">
-                
                     {lists}    
                 </div>
-                
             </div>
         )
     }
